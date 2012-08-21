d21cfb58a31f93231fc4489e1199317207f61885
Adam Honoré <Adam.Honore@Gmail.com>
2012-08-21 23:37:18 +0200

    updated Android library. pretty basic, still needs work.

---
+++ src/io/nb/domainr/api/Domainr.java
@@ -0,0 +1,57 @@
+package io.nb.domainr.api;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Domainr {
+
+    public static JSONObject search(String query) throws IOException, JSONException {
+        return request("http://domai.nr/api/json/search?q=", query);
+    }
+
+    public static JSONObject info(String query) throws IOException, JSONException {
+        return request("http://domai.nr/api/json/info?q=", query);
+    }
+
+    private static JSONObject request(String url, String query) throws IOException, JSONException {
+
+        HttpURLConnection connection = null;
+        BufferedReader bufferedReader = null;
+        StringBuilder stringBuilder = null;
+        String line = null;
+        JSONObject response = null;
+
+        try {
+            // Connect to the server and fetch the response
+            URL address = new URL(url + query);
+            connection = (HttpURLConnection) address.openConnection();
+            bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            stringBuilder = new StringBuilder();
+
+            // Convert the response to a string
+            while ((line = bufferedReader.readLine()) != null)
+                stringBuilder.append(line);
+
+            // Convert the string response to a JSONObject
+            response = new JSONObject(stringBuilder.toString());
+        }
+
+        // Close up the connection, and release all buffers
+        finally {
+            bufferedReader = null;
+            stringBuilder = null;
+            if (connection != null)
+                connection.disconnect();
+            connection = null;
+        }
+
+        // Return the response parsed to json
+        return response;
+    }
+}
